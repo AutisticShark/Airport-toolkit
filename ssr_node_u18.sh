18c9273eb3c7bff0a01019898b2ccbfad64bc364
Suicidal Cat <14369594+SuicidalCat@users.noreply.github.com>
2018-11-26 09:58:07 +0800

    Update ssr_node_u18.sh

--- ssr_node_u18.sh
+++ ssr_node_u18.sh
@@ -216,6 +216,6 @@ EOF
 sysctl -p
 echo "Setting startup script..."
 ln -fs /lib/systemd/system/rc-local.service /etc/systemd/system/rc-local.service
-wget -O rc.local https://raw.githubusercontent.com/YihanH/ss-panel-mod-v3-backend-server-install-scripts/master/rc.local_ubuntu_18 && chmod +x rc.local
+wget -O rc.local https://raw.githubusercontent.com/SuicidalCat/Airport-toolkit/master/rc.local.u18 && chmod +x rc.local
 mv -f rc.local /etc
 echo "Installation complete, please run python /soft/shadowsocks/server.py to test."
