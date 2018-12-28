74b6d5be787cdd11d392075c0378f8658cbd8240
SuicidalCat <14369594+SuicidalCat@users.noreply.github.com>
2018-12-29 03:21:58 +0800

    Update ssr_node_c7.sh

--- ssr_node_c7.sh
+++ ssr_node_c7.sh
@@ -194,39 +194,63 @@ if [[ ${is_auto} != "y" ]]; then
 		do_glzjinmod
 	fi
 fi
-echo "Running system optimization and enable BBR..."
-rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-yum remove kernel-headers -y
-yum --enablerepo=elrepo-kernel install kernel-ml kernel-ml-headers -y
-grub2-set-default 0
-echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
-cat >> /etc/security/limits.conf << EOF
-* soft nofile 51200
-* hard nofile 51200
+while :; do echo
+		echo -n "Do you want to enable BBR feature(from mainline kernel) and optimizate the system?(Y/N)"
+		read is_bbr
+		if [[ ${is_bbr} != "y" && ${is_bbr} != "Y" && ${is_bbr} != "N" && ${is_bbr} != "n" ]]; then
+			echo -n "Bad answer! Please only input number Y or N"
+		elif [[ ${is_bbr} == "y" || ${is_bbr} == "Y" ]]; then
+			do_bbr
+		else
+			break
+		fi
+done
+while :; do echo
+		echo -n "Do you want to register SSR Node as system service?(Y/N)"
+		read is_service
+		if [[ ${is_service} != "y" && ${is_service} != "Y" && ${is_service} != "N" && ${is_service} != "n" ]]; then
+			echo -n "Bad answer! Please only input number Y or N"
+		elif [[ ${is_service} == "y" || ${is_service} == "Y" ]]; then
+			do_service
+		else
+			break
+		fi
+done
+do_bbr(){
+	echo "Running system optimization and enable BBR..."
+	rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+	rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+	yum remove kernel-headers -y
+	yum --enablerepo=elrepo-kernel install kernel-ml kernel-ml-headers -y
+	grub2-set-default 0
+	echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
+	cat >> /etc/security/limits.conf << EOF
+	* soft nofile 51200
+	* hard nofile 51200
 EOF
-ulimit -n 51200
-cat >> /etc/sysctl.conf << EOF
-fs.file-max = 51200
-net.core.default_qdisc = fq
-net.core.rmem_max = 67108864
-net.core.wmem_max = 67108864
-net.core.netdev_max_backlog = 250000
-net.core.somaxconn = 4096
-net.ipv4.tcp_congestion_control = bbr
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_fin_timeout = 30
-net.ipv4.tcp_keepalive_time = 1200
-net.ipv4.ip_local_port_range = 10000 65000
-net.ipv4.tcp_max_syn_backlog = 8192
-net.ipv4.tcp_max_tw_buckets = 5000
-net.ipv4.tcp_fastopen = 3
-net.ipv4.tcp_rmem = 4096 87380 67108864
-net.ipv4.tcp_wmem = 4096 65536 67108864
-net.ipv4.tcp_mtu_probing = 1
+	ulimit -n 51200
+	cat >> /etc/sysctl.conf << EOF
+	fs.file-max = 51200
+	net.core.default_qdisc = fq
+	net.core.rmem_max = 67108864
+	net.core.wmem_max = 67108864
+	net.core.netdev_max_backlog = 250000
+	net.core.somaxconn = 4096
+	net.ipv4.tcp_congestion_control = bbr
+	net.ipv4.tcp_syncookies = 1
+	net.ipv4.tcp_tw_reuse = 1
+	net.ipv4.tcp_fin_timeout = 30
+	net.ipv4.tcp_keepalive_time = 1200
+	net.ipv4.ip_local_port_range = 10000 65000
+	net.ipv4.tcp_max_syn_backlog = 8192
+	net.ipv4.tcp_max_tw_buckets = 5000
+	net.ipv4.tcp_fastopen = 3
+	net.ipv4.tcp_rmem = 4096 87380 67108864
+	net.ipv4.tcp_wmem = 4096 65536 67108864
+	net.ipv4.tcp_mtu_probing = 1
 EOF
-sysctl -p
+	sysctl -p
+}
 echo "System require a reboot to complete the installation process, press Y to continue, or press any key else to exit this script."
 read is_reboot
 if [[ ${is_reboot} == "y" || ${is_reboot} == "Y" ]]; then
