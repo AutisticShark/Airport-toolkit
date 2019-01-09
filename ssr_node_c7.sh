7cd5352c1d6e14e5588e9fe182b034ef103390e9
SuicidalCat <14369594+SuicidalCat@users.noreply.github.com>
2019-01-10 00:46:39 +0800

    Update ssr_node_c7.sh

--- ssr_node_c7.sh
+++ ssr_node_c7.sh
@@ -94,7 +94,7 @@ cd /soft
 echo "Checking if there any exist Shadowsocksr server software..."
 if [ ! -d "shadowsocks" ]; then
 	echo "Installing Shadowsocksr server from GitHub..."
-	cd /tmp && git clone -b manyuser https://github.com/esdeathlove/shadowsocks.git
+	cd /tmp && git clone -b manyuser https://github.com/NimaQu/shadowsocks.git
 	mv -f shadowsocks /soft
 else
 	while :; do echo
