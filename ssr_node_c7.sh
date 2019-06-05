e1feab77b3fd23f0c9865054f244198930c9a50f
Toxic Cat <14369594+Toxic-Cat@users.noreply.github.com>
2019-06-05 09:12:17 +0800

    Update ssr_node_c7.sh

--- ssr_node_c7.sh
+++ ssr_node_c7.sh
@@ -81,8 +81,8 @@ systemctl stop firewalld && systemctl disable firewalld
 echo "Setting system timezone..."
 timedatectl set-timezone Asia/Taipei && systemctl stop ntpd.service && ntpdate us.pool.ntp.org
 echo "Installing libsodium..."
-wget https://github.com/jedisct1/libsodium/releases/download/1.0.17/libsodium-1.0.17.tar.gz
-tar xf libsodium-1.0.17.tar.gz && cd libsodium-1.0.17
+wget https://github.com/jedisct1/libsodium/releases/download/1.0.18/libsodium-1.0.18.tar.gz
+tar xf libsodium-1.0.18.tar.gz && cd libsodium-1.0.18
 ./configure && make -j2 && make install
 echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 ldconfig
