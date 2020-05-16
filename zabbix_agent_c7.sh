36abb13bf257ce344af07e928eb08af6d4dbeea5
Toxic Cat <14369594+Toxic-Cat@users.noreply.github.com>
2020-05-16 19:25:50 +0800

    Update zabbix_agent_c7.sh

--- zabbix_agent_c7.sh
+++ zabbix_agent_c7.sh
@@ -13,13 +13,14 @@ cat << "EOF"
 Author: Toxic Cat
 Github: https://github.com/Toxic-Cat/Airport-toolkit                                 
 EOF
-echo "Zabbix installation script for CentOS 7 x64"
+echo "Zabbix agent installation script for CentOS 7 x64"
 
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script!"; exit 1; }
 
 #Configuration
 zabbix_release_40_url = http://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-2.el7.noarch.rpm
 zabbix_release_50_url = http://repo.zabbix.com/zabbix/5.0/rhel/7/x86_64/zabbix-release-5.0-1.el7.noarch.rpm
+zabbix_config_file_path = /etc/zabbix/zabbix_agentd.conf
 
 while :; do echo
 	echo -e "Please select Zabbix agent version you want to install:"
@@ -42,7 +43,12 @@ do_zabbix_agent_configure(){
 }
 
 do_edit_zabbix_agent_config(){
-    sed -i -e "s/Server=127.0.0.1/Server=$zabbix_master_ip/g" -e "s/ServerActive=127.0.0.1/ServerActive=$zabbix_master_ip/g" -e "s/Hostname=Zabbix server/Hostname=$agent_hostname/g" /etc/zabbix/zabbix_agentd.conf
+    sed -i -e "s/Server=127.0.0.1/Server=$zabbix_master_ip/g" -e "s/ServerActive=127.0.0.1/ServerActive=$zabbix_master_ip/g" -e "s/Hostname=Zabbix server/Hostname=$agent_hostname/g" $zabbix_config_file_path
+}
+
+do_enable_and_start_zabbix_agent(){
+    systemctl enable zabbix-agent
+    systemctl restart zabbix-agent
 }
 
 do_install_zabbix_agent_40(){
@@ -54,3 +60,13 @@ do_install_zabbix_agent_50(){
     rpm -ivh $zabbix_release_50_url
     yum install zabbix-agent -y
 }
+
+do_zabbix_agent_configure
+
+if [[ ${zabbix_version} == 1 ]]; then
+    do_install_zabbix_agent_40
+elif [[ ${zabbix_version} == 2 ]]; then
+    do_install_zabbix_agent_50
+fi
+
+do_enable_and_start_zabbix_agent
