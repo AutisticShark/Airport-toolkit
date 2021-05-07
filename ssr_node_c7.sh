f45d910fc077aa12455cb178be4d9d787e0c9869
兩足大貓貓 <14369594+M1Screw@users.noreply.github.com>
2021-05-07 08:12:47 -0400

    更換分支至 manyuser

--- ssr_node_c7.sh
+++ ssr_node_c7.sh
@@ -77,7 +77,7 @@ echo "Installing Python3.6..."
 yum install python36 python36-pip -y
 echo "Installing Shadowsocksr server from GitHub..."
 mkdir /soft
-cd /tmp && git clone -b testing https://github.com/Anankke/shadowsocks-mod.git
+cd /tmp && git clone -b manyuser https://github.com/Anankke/shadowsocks-mod.git
 mv shadowsocks-mod shadowsocks
 mv -f shadowsocks /soft
 cd /soft/shadowsocks
