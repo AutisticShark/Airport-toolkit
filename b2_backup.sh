f4309d5a46d20ed857b582fde7a1784dbd868ead
兩足大貓貓 <14369594+M1Screw@users.noreply.github.com>
2021-06-12 02:53:56 -0400

    Update b2_backup.sh

--- b2_backup.sh
+++ b2_backup.sh
@@ -28,11 +28,14 @@ do_pre_config(){
 
 do_db_export(){
     if [[ ${compress_method} == "gzip" ]]; then
-        db_file_name=$(date +'%d-%m-%Y-%H-%M-%S')-db.tar.gz
-        mysqldump -u $db_user -p$db_password -h $db_host $db_name | gzip -c > $db_file_name
+        db_file_sql=$(date +'%d-%m-%Y-%H-%M-%S').sql
+        db_file_name=$(date +'%d-%m-%Y-%H-%M-%S')-db.gz
+        mysqldump -u $db_user -p$db_password -h $db_host $db_name > $db_file_sql
+        gzip -c $db_file_sql > $db_file_name
+        rm $db_file_sql
     elif [[ ${compress_method} == "zip" ]]; then
         db_file_name=$(date +'%d-%m-%Y-%H-%M-%S')-db.zip
-        mysqldump -u $db_user -p$db_password -h $db_host $db_name | zip > $db_file_name
+        mysqldump -u $db_user -p$db_password -h $db_host $db_name | zip -qq > $db_file_name
     else
         echo -n "Unknown compress method"
         exit 0
@@ -45,7 +48,7 @@ do_pack_website(){
         tar -czf $website_file_name $website_dir
     elif [[ ${compress_method} == "zip" ]]; then
         website_file_name=$(date +'%d-%m-%Y-%H-%M-%S')-web.zip
-        zip -r $website_file_name $website_dir
+        zip -rqq $website_file_name $website_dir
     else
         echo -n "Unknown compress method"
         exit 0
@@ -56,6 +59,8 @@ do_upload_b2(){
     b2 authorize-account $b2_app_key_id $b2_app_key
     b2 upload_file $b2_bucket_name $(pwd)/$website_file_name $website_file_name
     b2 upload_file $b2_bucket_name $(pwd)/$db_file_name $db_file_name
+    rm $website_file_name
+    rm $db_file_name
 }
 
 if [[ $1 == "config" ]]; then
