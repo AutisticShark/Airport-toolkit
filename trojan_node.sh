a924aa95b3a01b58500cf48bfec4e7ea333d8dff
Cat <14369594+M1Screw@users.noreply.github.com>
2022-07-30 15:00:36 +0800

    Add trojan_node.sh

---
+++ trojan_node.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+cat << "EOF"
+TrojanX server installation script for RHEL/Fedora                                                          
+Author: M1Screw
+Github: https://github.com/M1Screw/Airport-toolkit
+Usage: 
+./trojan_node_c9.sh install --> Install TrojanX server
+./trojan_node_c9.sh config --> Configure TrojanX server
+./trojan_node_c9.sh update --> Update TrojanX server                           
+EOF
+
+[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script!"; exit 1; }
+
+do_install_trojan_server(){
+    echo "Detecting CPU Architecture..."
+    cpu_arch=$(uname -m)
+    if [[ $cpu_arch != "x86_64" || $cpu_arch != "aarch64" ]]
+    then
+        echo "Error: Unsupported CPU Architecture!"
+        exit 1
+    fi
+    echo "Detecting OS..."
+    os_release=$(cat /etc/os-release | grep "^ID=" | awk -F '=' '{print $2}')
+    os_version=$(cat /etc/os-release | grep "^VERSION_ID=" | awk -F '=' '{print $2}')
+    if [[ $os_release != "rhel" || $os_release != "centos" || $os_release != "rocky" || $os_release != "fedora" ]]
+    then
+        echo "Error: Unsupported OS!"
+        exit 1
+    fi
+    if [[ $os_release == "rhel" || $os_release != "centos" || $os_release != "rocky" ]]
+    then
+        if [[ $os_version != "8"* ||  $os_version != "9"* ]]
+        then
+            echo "Error: Unsupported OS version!"
+            exit 1
+        fi
+    fi
+    if [ $os_release == "fedora" ]
+    then
+        if [[ $os_version != "34" || $os_version != "35" || $os_version != "36" ]]
+        then
+            echo "Error: Unsupported OS version!"
+            exit 1
+        fi
+    fi
+    echo "Installing dependency && Updating current installed package..."
+    dnf update -y
+    dnf install wget -y
+    echo "Installing DNF repo..."
+    wget -O /etc/yum.repos.d/sspanel.repo https://mirror.sspanel.org/repo/rhel.repo
+    echo "Installing TrojanX server..."
+    dnf install trojan-server -y
+    echo "Installing TrojanX server..."
+}
+do_install_acme(){
+    read -p "Please input your email: " email
+    echo "Installing acme..."
+    wget -O -  https://get.acme.sh | sh -s $email
+}
+do_config(){
+    read -p "Please input your node port(443): " node_port
+    read -p "Please input your node id: " node_id
+    read -p "Please input your mu key: " mu_key
+    read -p "Please input your panel url(https://example.com/mod_mu): " panel_url
+    while :; do echo
+	    echo -e "Please select your cert mode:"
+	    echo -e "\t1. Manual"
+	    echo -e "\t2. Auto - Cloudflare DNS"
+        echo -e "\t3. Auto - Amazon Route53 DNS"
+        echo -e "\t4. Auto - DNSPod DNS"
+        echo -e "\t5. Auto - Aliyun DNS"
+	    read -p "Please input a number:(Default 2 press Enter) " cert_mode
+	    [ -z ${cert_mode} ] && cert_mode=2
+	    if [[ ! ${cert_mode} =~ ^[1-5]$ ]]; then
+		    echo "Bad answer! Please only input number 1~5"
+	    else
+		    if [[ ${cert_mode} == 1 ]]; then
+                read -p "Please input your domain name: " domain
+                read -p "Please input your cert path(/path/to/fullchain_cert.pem): " cert_path
+                read -p "Please input your key path(/path/to/cert.key): " key_path
+                cp $cert_path /etc/trojan-server/cert.pem
+                cp $key_path /etc/trojan-server/cert.key
+                break
+            elif [[ ${cert_mode} == 2 ]]; then
+                read -p "Please input your domain name: " domain
+                read -p "Please input your Cloudflare key: " cf_key
+                read -p "Please input your Cloudflare email: " cf_email
+                CF_Key=$cf_key CF_Email=$cf_email acme.sh --issue --dns dns_cf -d $domain
+                acme.sh --install-cert -d $domain --key-file /etc/trojan-server/cert.key --fullchain-file /etc/trojan-server/cert.pem
+                break
+            elif [[ ${cert_mode} == 3 ]]; then
+                read -p "Please input your domain name: " domain
+                read -p "Please input your Amazon Route53 access key id: " route53_key_id
+                read -p "Please input your Amazon Route53 access key: " route53_key
+                AWS_ACCESS_KEY_ID=$route53_key_id AWS_SECRET_ACCESS_KEY=$route53_key acme.sh --issue --dns dns_aws -d $domain
+                acme.sh --install-cert -d $domain --key-file /etc/trojan-server/cert.key --fullchain-file /etc/trojan-server/cert.pem
+                break
+            elif [[ ${cert_mode} == 4 ]]; then
+                read -p "Please input your domain name: " domain
+                read -p "Please input your DNSPod id: " dnspod_id
+                read -p "Please input your DNSPod key: " dnspod_key
+                DP_Id=$dnspod_id DP_Key=$dnspod_key acme.sh --issue --dns dns_dp -d $domain
+                acme.sh --install-cert -d $domain --key-file /etc/trojan-server/cert.key --fullchain-file /etc/trojan-server/cert.pem
+                break
+            elif [[ ${cert_mode} == 5 ]]; then
+                read -p "Please input your domain name: " domain
+                read -p "Please input your Aliyun key: " aliyun_key
+                read -p "Please input your Aliyun secret: " aliyun_secret
+                Ali_Key=$aliyun_key Ali_Secret=$aliyun_secret acme.sh --issue --dns dns_ali -d $domain
+                acme.sh --install-cert -d $domain --key-file /etc/trojan-server/cert.key --fullchain-file /etc/trojan-server/cert.pem
+                break
+            fi
+	    fi			
+    done
+    sed -i -e 's/0.0.0.0:443/0.0.0.0:$node_port/g' -e 's/"id": 1/"id": $node_id/g' -e 's/example-key/$mu_key/g' -e 's|https://example.com/mod_mu|$panel_url|g' -e 's/"example.com":/"$domain":/g' -e 's/example.pem/cert.pem/g' -e 's/example.key/cert.key/g' /etc/trojan-server/sspanel.json
+    systemctl restart trojan-server
+    systemctl enable trojan-server
+}
+do_update(){
+    echo "Updating SSPanel-UIM RPM Repository..."
+    wget -O /etc/yum.repos.d/sspanel.repo https://mirror.sspanel.org/repo/rhel.repo
+    echo "Updating TrojanX server..."
+    dnf update trojan-server -y
+    echo "Updating acme.sh..."
+    acme.sh --upgrade
+}
